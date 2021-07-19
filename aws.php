0e1f15afb63b84c8eee8f47632e265277e09dfec
Eric Heng <41599815+3richeng@users.noreply.github.com>
2021-07-19 12:23:14 +0800

    Update aws.php

--- aws.php
+++ aws.php
@@ -1,7 +1,7 @@
 <?php
 
     //AWS Keys
-    $BUCKET_NAME = 'mmu-fyp-bucket';
-    $IAM_KEY = 'AKIAIT56AESOWJURE6JA';
-    $IAM_SECRET = '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';
+    $BUCKET_NAME = '';
+    $IAM_KEY = '';
+    $IAM_SECRET = '';
 
